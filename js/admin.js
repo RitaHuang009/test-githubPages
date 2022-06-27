5fc42c7d8162742c090d5b497c73f70785b5d4ca
ritahuang <ritahuang@hitrust.com.tw>
2022-06-27 17:50:53 +0800

    新增圓餅圖銷售量排序顯示

--- js/admin.js
+++ js/admin.js
@@ -1,12 +1,71 @@
+let orderData = [];
+
+const orderList = document.querySelector('.js-orderList');
+
 function initAdmin() {
     getOrderList()
 }
 
 initAdmin()
 
-let orderData = [];
+function renderC3_LV2() {
+    let obj = {};
+    orderData.forEach(function (item) {
+        item.products.forEach(function (productItem) {
+            if (obj[productItem.title] == undefined) {
+                obj[productItem.title] = productItem.quantity * productItem.price;
+            } else {
+                obj[productItem.title] += productItem.quantity * productItem.price;
+            }
+        })
+    })
 
-const orderList = document.querySelector('.js-orderList');
+    // 重組為c3格式
+    let originAry = Object.keys(obj);
+    let newData = [];
+    originAry.forEach(function (item) {
+        let ary = [];
+        ary.push(item);
+        ary.push(obj[item])
+        newData.push(ary)
+    })
+
+    // 依照品項銷售量排序 [高 -> 低]
+    newData.sort(function(a,b){
+        return b[1] - a[1]
+    })
+
+    if(newData.length > 3){
+        let otherTotal = 0;
+        newData.forEach(function(item,index){
+            if(index > 2){
+                otherTotal += newData[index][1]
+            }
+        })
+        newData.splice(3)
+        newData.push(['其他',otherTotal])
+        console.log(newData);
+    }
+
+
+    const chart = c3.generate({
+        bindto: "#chart",
+        data: {
+            columns: newData,
+            type: 'donut',
+        },
+        donut: {
+            title: "銷售量"
+        },
+        color: {
+            pattern: ["#301E5F", "#5434A7", "#9D7FEA", "#DACBFF"]
+        }
+    });
+}
+
+
+
+
 
 function getOrderList() {
     axios.get(`https://livejs-api.hexschool.io/api/livejs/v1/admin/${api_path}/orders`, {
@@ -21,8 +80,8 @@ function getOrderList() {
             orderData.forEach(function (item) {
                 // 組日期字串 
                 const timeStamp = new Date(item.createdAt * 1000);
-                console.log(timeStamp);
                 const orderTime = `${timeStamp.getFullYear()}/${timeStamp.getMonth() + 1}/${timeStamp.getDay()}`
+
                 // 組購買項目的字串
                 let productStr = '';
                 item.products.forEach(function (productItem) {
@@ -37,26 +96,28 @@ function getOrderList() {
                 }
 
                 str += `<tr>
-                  <td>${item.id}</td>
-                  <td>
-                      <p>${item.user.name}</p>
-                      <p>${item.user.tel}</p>
-                  </td>
-                  <td>${item.user.address}</td>
-                  <td>${item.user.email}</td>
-                  <td>
-                  ${productStr}
-                  </td>
-                  <td>${orderTime}</td>
-                  <td class="orderStatus">
-                      <a href="#" data-status="${item.paid}" class="orderStatus" data-id="${item.id}">${orderStatus}</a>
-                  </td>
-                  <td>
-                      <input type="button" class="delSingleOrder-Btn js-orderDelete" data-id="${item.id}" value="刪除">
-                  </td>
-          </tr>`
+                    <td>${item.id}</td>
+                    <td>
+                        <p>${item.user.name}</p>
+                        <p>${item.user.tel}</p>
+                    </td>
+                    <td>${item.user.address}</td>
+                    <td>${item.user.email}</td>
+                    <td>
+                    ${productStr}
+                    </td>
+                    <td>${orderTime}</td>
+                    <td class="orderStatus">
+                        <a href="#" data-status="${item.paid}" class="orderStatus" data-id="${item.id}">${orderStatus}</a>
+                    </td>
+                    <td>
+                        <input type="button" class="delSingleOrder-Btn js-orderDelete" data-id="${item.id}" value="刪除">
+                    </td>
+            </tr>`
             })
+            console.log(orderData);
             orderList.innerHTML = str;
+            renderC3_LV2()
         })
 }
 
